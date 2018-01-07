3d5cf9063449a4f9ee3b387e73eadd4463746c86
chelovekdrakon <MorauFiodar@yahoo.com>
2018-01-07 20:03:41 +0300

    Algorithms/feature: Shell sort

--- src/SortingAlgorithms/ArrayTestBed.js
+++ src/SortingAlgorithms/ArrayTestBed.js
@@ -1,3 +1,4 @@
+
 export default class CArray {
     constructor(numElements) {
         this.dataStore = [];
@@ -7,6 +8,8 @@ export default class CArray {
         for (let i = numElements; i > 0; i--) {
             this.dataStore.push(i);
         }
+
+        this.gaps = [];
     }
 
     setData() {
@@ -36,8 +39,9 @@ export default class CArray {
 
     bubbleSort() {
         // window.console.log(`init array: ${this.toString()}`);
+        const len = this.numElements;
 
-        for (let outer = this.numElements; outer >= 0; outer--) {
+        for (let outer = len; outer >= 0; outer--) {
 
             for (let inner = 0; inner < outer; inner++) {
                 if (this.dataStore[inner] > this.dataStore[inner + 1]) {
@@ -50,11 +54,12 @@ export default class CArray {
 
     selectionSort() {
         // window.console.log(`init array: ${this.toString()}`);
+        const len = this.numElements;
 
-        for (let outer = 0; outer < this.numElements - 1; outer++) {
+        for (let outer = 0; outer <= len; outer++) {
             let min = outer;
 
-            for (let inner = outer + 1; inner < this.numElements; inner++) {
+            for (let inner = outer + 1; inner < len; inner++) {
                 if (this.dataStore[inner] < this.dataStore[min]) {
                     min = inner;
                 }
@@ -67,15 +72,41 @@ export default class CArray {
     insertionSort() {
         // window.console.log(`init array: ${this.toString()}`);
 
-        for (let outer = 1; outer < this.numElements; outer++) {
+        const len = this.numElements;
+
+        for (let outer = 1; outer < len; outer++) {
+            let value = this.dataStore[outer];
             let inner = outer;
 
-            while (inner > 0 && this.dataStore[inner - 1] >= this.dataStore[outer]) {
-                this.dataStore[inner] = this.dataStore[inner - 1];
+            while (inner > 0 && this.dataStore[inner - 1] > value) {
+                this.swap(inner, inner - 1);
                 inner--;
             }
             // window.console.log(`Passage number ${outer}: ${this.toString()}`);
         }
+    }
+
+    shellSort() {
+        this.gaps.forEach( gap => {
+
+            const len = this.numElements;
+
+            for (let outer = gap; outer < len; outer += gap) {
+                let value = this.dataStore[outer];
+                let inner = outer;
+
+                while (inner >= gap && this.dataStore[inner - gap] > value) {
+                    this.swap(inner, inner - gap);
+                    inner--;
+                }
+                // window.console.log(`Passage number ${outer}: ${this.toString()}`);
+            }
+
+        });
+    }
+
+    setGaps(arr) {
+        this.gaps = arr;
     }
 
 }
--- src/SortingAlgorithms/index.js
+++ src/SortingAlgorithms/index.js
@@ -2,35 +2,43 @@ import CArray from './ArrayTestBed';
 
 export function testAlgorithms() {
 
-    const numElements = 10000;
+    const numElements = 1000;
     const myNums = new CArray(numElements);
     myNums.setData();
     // myNums.clear();
-
+    window.console.log(myNums);
 
     // myNums.bubbleSort();
     // myNums.selectionSort();
     // myNums.insertionSort();
 
+    // let start = Date.now();
+    // myNums.insertionSort();
+    // let stop = Date.now();
+    // let timeDiff = stop - start;
+    // window.console.log(timeDiff / 1000);
+
+    // let start = Date.now();
+    // myNums.selectionSort();
+    // let stop = Date.now();
+    // let timeDiff = stop - start;
+    // window.console.log(timeDiff / 1000);
+    //
+    // let start = Date.now();
+    // myNums.bubbleSort();
+    // let stop = Date.now();
+    // let timeDiff = stop - start;
+    // window.console.log(timeDiff / 1000);
+
+    const MARCIN_CIURA_GAPS = [701, 301, 132, 57, 23, 10, 4, 1];
+    const MY_GAPS = [5, 3, 1];
+    myNums.setGaps(numElements >= 100000 ? MARCIN_CIURA_GAPS : MY_GAPS);
+
     let start = Date.now();
-    myNums.insertionSort();
+    myNums.shellSort();
     let stop = Date.now();
     let timeDiff = stop - start;
     window.console.log(timeDiff / 1000);
 
-    start = Date.now();
-    myNums.selectionSort();
-    stop = Date.now();
-    timeDiff = stop - start;
-    window.console.log(timeDiff / 1000);
-
-    start = Date.now();
-    myNums.bubbleSort();
-    stop = Date.now();
-    timeDiff = stop - start;
-    window.console.log(timeDiff / 1000);
-
-
-    window.console.log(myNums);
 
 }
