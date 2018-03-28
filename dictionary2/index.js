8ddfa4567d20dd7ca8cb92670b33939ec6dc47b1
chelovekdrakon <fiodar.morau@sharekey.com>
2018-03-28 09:45:59 +0300

    feature/dictionary: final dictionary JSON -> result.txt

--- dictionary2/index.js
+++ dictionary2/index.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 // const wordListPath = require('word-list');
 // const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
 
-const wordArray = fs.readFileSync('./wow.txt', 'utf8').split('\n');
+const wordArray = fs.readFileSync('./frequentlyUsed.txt', 'utf8').split('\n');
 const suitableWords = wordArray.filter(word => word.length >= 3 && word.length <= 8);
 
-const stream = fs.createWriteStream('./wowSuit.txt', {encoding: 'utf8'});
-stream.write(suitableWords.join('\r\n'));
+const stream = fs.createWriteStream('./result.json', {encoding: 'utf8'});
+stream.write(JSON.stringify(suitableWords));
 stream.end();
